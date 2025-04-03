dc15e1fdbf34f059ad70ac3f4c8190c30a637091
Neo3691102 <nestor8772@gmail.com>
2025-04-03 17:11:26 -0600

    Actualizacion de codigoJS

--- script.js
+++ script.js
@@ -4,7 +4,32 @@ const $d = document;
 const $sneakers = $d.getElementById("sneakers");
 const $template = $d.getElementById("sneaker-template");
 const $fragmento = $d.createDocumentFragment();
-const $options = {header: {Authorization: `Baerer ${KEYS.secret}`}}
+const $options = { headers: { Authorization: `Bearer ${KEYS.secret}` } };
 
-let products, price;
+let products, prices;
 
+Promise.all([
+  fetch("https://api.stripe.com/v1/products", $options), //url de 1er peticiion
+  fetch("https://api.stripe.com/v1/prices", $options), // url de 2da peticion sacada de la documentacion
+])
+.then(responses => console.log(responses.map(res => res.json())))
+.then(json => {
+    products = json[0].data;
+    prices = json[1].data;
+ 
+    prices.forEach(el => {
+        let productData = products.filter(product => product.id === el.product);
+        console.log(productData);
+
+        $template.querySelector("sneaker").setAttribute("data-price", el.id);
+        $template.querySelector("img").setAttribute.src = productData[0].images[0];
+        $template.querySelector("img").alt = productData[0].name;
+        $template.querySelector("figcaption").innerHTML = `${productData[0].name} ${el.unit_amount_decimal} ${el.currency}`;
+
+        let $clone = $d.importNode($template, true);
+
+        $fragmento.appendChild($clone);
+    });
+
+    $sneakers.appendChild($fragmento);
+})
