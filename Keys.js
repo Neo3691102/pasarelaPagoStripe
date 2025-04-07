00d576445e240f557af45020657cf33a9956ed46
Neo3691102 <nestor8772@gmail.com>
2025-04-07 17:08:50 -0600

    Actualizacion de codigoJS

--- Keys.js
+++ Keys.js
@@ -1,4 +1,6 @@
 export default {
-    public: "tu clave publica va aqui",
-    secret: "tu clave secreta va aqui"
-}
+  public:
+    "pk_test_51R8rb4RrKl9M6ouqtZHJbGjukfVGgoICGgFl2Y07eX9M3MFmM2C7VzXv0B1QwixPH8Vrw7E0SjRvsbsVsc8OBjTA00PowAquvH",
+  secret:
+    "sk_test_51R8rb4RrKl9M6ouqZotj2N01hp8NGcuNJN9sbeGDOaHYMDfkgPRgrhrc0isIVa6jROEsgsDuvCbpkLI86iZWmqNN00ruVxmQu6",
+};
